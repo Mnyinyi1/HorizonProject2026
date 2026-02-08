0261ed0825197a7d246aded92446b2a46140f181
mnyinyi <mnyinyi20@gmail.com>
2026-02-08 18:23:41 +0200

    created the project structure

---
+++ src/test/java/utils/StartChrome.java
@@ -0,0 +1,15 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class StartChrome {
+    private WebDriver drive;
+
+    public WebDriver getDrive() {
+        return drive;
+    }
+
+    public void setDrive(WebDriver drive) {
+        this.drive = drive;
+    }
+}
